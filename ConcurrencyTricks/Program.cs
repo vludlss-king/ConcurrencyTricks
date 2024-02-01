933132aeeaafb5f2a38e715a448a8b52b2650a2d
vludlss <vladislav.mikkoev@yandex.ru>
2024-02-01 05:36:54 +0300

    feat: "Мягкий" таймаут

---
+++ ConcurrencyTricks/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConcurrencyTricks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ ConcurrencyTricks/SoftTimeout.cs
@@ -0,0 +1,37 @@
+﻿namespace ConcurrencyTricks
+{
+    /// <summary>
+    /// "Мягкий таймаут"
+    /// </summary>
+    internal static class SoftTimeout
+    {
+        /// <summary>
+        /// Версия в случае если методом не предусмотрен таймаут через CancellationToken
+        /// </summary>
+        /// <param name="uri">Путь к файлу</param>
+        public static async Task<string?> ReadFileWithTimeout1(string uri)
+        {
+            using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+            Task<string> readTask = File.ReadAllTextAsync(uri);
+            Task timeoutTask = Task.Delay(Timeout.Infinite, cts.Token);
+
+            Task completedTask = await Task.WhenAny(readTask, timeoutTask);
+            if (completedTask == timeoutTask)
+                return null;
+            return await readTask;
+        }
+
+        /// <summary>
+        /// Версия в случае, если методом предусмотрен таймаут через CancellationToken
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <returns></returns>
+        public static async Task<string> ReadFileWithTimout2(string uri)
+        {
+            using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+            string fileText = await File.ReadAllTextAsync(uri, cts.Token);
+
+            return fileText;
+        }
+    }
+}
